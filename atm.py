1dc554f7634d222fbf9b6a22278c836d0384535f
CJY9697 <jy970114@naver.com>
2025-03-12 18:07:05 +0900

    ✨ 입금 금액 추가
    사용자 입금 금액을 입력받고, 원금에 추가하는 기능 구현, 입금액과 잔액 출력 기능 구현, 문자 형태나 음수 형태로 입력방지 조건 구현

--- atm.py
+++ atm.py
@@ -1,15 +1,19 @@
 # balance : 통장에 들어있는 기본 금액을 담는 변수
 # 1.입금, 2.출금, 3.영수증보기,4.종료 ==> 숫자로 원하는 기능을 입력
 # 사용자가 입력한 기능은 num 변수에 달아주세요
-
+# deposit_amount: 
 balance = 3000
 
 while True:
     num =input("사용하실 기능의 번호를 입력해주세요.(1.입금, 2.출금, 3. 영수증보기, 4. 종료)")
     
     if num =="1":
-        print("입금입니다.")
-
+        deposit_amount = input("입금할 금액을 입력해주세요. : ")
+        if deposit_amount.isdigit() and int(deposit_amount) > 0:
+            balance += int(deposit_amount)
+            print(f'고객님의 입금 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+        else:
+            print("정신차리고,제대로된 숫자형태로 입금액을 작성해줘!")
     if num =="2":
         pass
     if num =="3":
